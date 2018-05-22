03086b4969d1208b6a091d9de0877cb4ffd21bb2
David Shams <david.shams@gmail.com>
2018-05-21 22:29:24 -0500

    FormControl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  customerForm: FormGroup;
+  customer: Customer = new Customer(); // data model
+  ngOnInit() {
+    this.customerForm = new FormGroup({  // Root FormGroup for the model
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      email: new FormControl(),
+      sendCatalog: new FormControl(true)
+
+    });
+  }
+}
+class Customer{
+  constructor(){}
 }
